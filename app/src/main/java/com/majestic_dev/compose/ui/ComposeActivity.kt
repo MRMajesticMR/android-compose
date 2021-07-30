8d5aa52cbf1b66be9694652ff48b6c1000e41901
arkadiy.zakharov <arkadiy,zakharov@doconcall.ru>
2021-07-30 18:45:51 +0300

    Try out Row and Column layout

--- app/src/main/java/com/majestic_dev/compose/ui/ComposeActivity.kt
+++ app/src/main/java/com/majestic_dev/compose/ui/ComposeActivity.kt
@@ -3,11 +3,20 @@ package com.majestic_dev.compose.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.majestic_dev.compose.R
 import com.majestic_dev.compose.ui.theme.ComposeTheme
 
 class ComposeActivity : ComponentActivity() {
@@ -16,19 +25,49 @@ class ComposeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MessageCard(name = "Android")
+            MessageCard(
+                msg = Message(
+                    author = "Anton",
+                    body = "Here some text"
+                )
+            )
         }
 
     }
 
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello $name")
+    fun MessageCard(msg: Message) {
+        Row(
+            modifier = Modifier
+                .padding(all = 9.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Author avatar",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = msg.author)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body)
+            }
+        }
     }
 
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(name = "IOS")
+        MessageCard(
+            msg = Message(
+                author = "Arkadii",
+                body = "Hello"
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/majestic_dev/compose/ui/Data.kt
@@ -0,0 +1,6 @@
+package com.majestic_dev.compose.ui
+
+class Message(
+    val author: String,
+    val body: String
+)
