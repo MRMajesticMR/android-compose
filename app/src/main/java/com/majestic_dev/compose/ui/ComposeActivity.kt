088945ea2f491d78e829d16cbf33e0614704c3ec
arkadiy.zakharov <arkadiy,zakharov@doconcall.ru>
2021-07-31 13:03:04 +0300

    Try out LazyColumn

--- app/src/main/java/com/majestic_dev/compose/ui/ComposeActivity.kt
+++ app/src/main/java/com/majestic_dev/compose/ui/ComposeActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -34,6 +36,16 @@ class ComposeActivity : ComponentActivity() {
                         body = "Here some text"
                     )
                 )
+            }
+        }
+
+    }
+
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(msg = message)
             }
         }
 
@@ -74,6 +86,14 @@ class ComposeActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        ComposeTheme {
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 
